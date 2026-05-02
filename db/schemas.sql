69f7d9cd7526dc6e4d91654e66fc175355588ccb
chiniu <kimpoychin131@gmail.com>
2026-05-02 23:52:27 +0800

    add constraints to schema

--- db/schemas.sql
+++ db/schemas.sql
@@ -10,10 +10,14 @@ CREATE TABLE users (
     rarity_chance NUMERIC(5,3) DEFAULT 0.00,
     loot_chance NUMERIC(5,3) DEFAULT 0.00,
     base_cooldown INT DEFAULT ((60 * 60) * 4),
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    CONSTRAINT check_gold CHECK(gold <= 9999),
+    CONSTRAINT rarity_check CHECK(rarity_chance <= 0.012),
+    CONSTRAINT loot_check CHECK(loot_chance <= 0.012),
+    CONSTRAINT cooldown_check CHECK(base_cooldown <= 28800)
 );
 
-CREATE TABLE cooldowns (
+ CREATE TABLE cooldowns (
     id SERIAL PRIMARY KEY,
     user_id VARCHAR(20) NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     command VARCHAR (50) NOT NULL,
@@ -32,5 +36,5 @@ UPDATE users
 
 UPDATE users 
     SET 
-       gold = gold + 9999
+       gold = gold + 8888
     WHERE user_id = '' AND username = ''
